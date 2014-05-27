dc70cfd45c9e580fcca58d71f24c829bf5c2dc77
Rolf Håvard Blindheim <rhblind@gmail.com>
2014-05-27 10:27:11 +0200

    removed `use_caching` kwarg to keep backward compatibility

--- weeny/signals.py
+++ weeny/signals.py
@@ -7,4 +7,4 @@ from django.dispatch import Signal
 # Sent when a visitor is redirected to the target URL,
 # and URL tracking is enabled for either the URL or the
 # weeny site the URL belongs to.
-track_visit = Signal(providing_args=["instance", "request"], use_caching=True)
+track_visit = Signal(providing_args=["instance", "request"])
